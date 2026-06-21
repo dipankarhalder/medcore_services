d88c07718f482a12aee498e417ab0c5f58c24299
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2026-06-21 13:18:02 +0530

    updated the controllers name

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from api.controllers.v1.health_controller import health_check as v1_health_check
-from api.controllers.v1.auth_controller import register as v1_register, login as v1_login, profile as v1_profile
+from api.controllers.v1.health_controller import health_check
+from api.controllers.v1.auth_controller import register, login, profile
 
 urlpatterns = [
-    path('v1/health-check/', v1_health_check, name='v1_health_check'),
-    path('v1/auth/register/', v1_register, name='v1_register'),
-    path('v1/auth/login/', v1_login, name='v1_login'),
-    path('v1/auth/profile/', v1_profile, name='v1_profile'),
+    path('v1/health-check/', health_check, name='health_check'),
+    path('v1/auth/register/', register, name='register'),
+    path('v1/auth/login/', login, name='login'),
+    path('v1/auth/profile/', profile, name='profile'),
 ]
